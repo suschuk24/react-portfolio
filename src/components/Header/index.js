e3a405f54cc8f10b3233380ef1f3a163de1c5e54
Seth Uschuk <suschuk24@gmail.com>
2020-12-13 17:23:20 -0600

    added body elements and css elements

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Work from '../Work'
 
 
 function Header() {
+
+    const [pages] = useState([
+        {
+            name: 'My Work',
+            description: "A showcase of applications I've built and contributed to"
+        },
+        {
+            name: 'Contact',
+            description: 'Please enter your information below with any questions, and I will get back to you as I can!'
+        },
+        {
+            name: "Resume",
+            description: 'an HTML version of my resume'
+        }
+    ]);
+
     return (
         <div className="App">
             <header className="App-header">
                 <h1>
                     <a href="/">Seth Uschuk's Portfolio</a>
                 </h1>
-                <ul>
-                    <li>
-                        <a href="#about-me">About Me</a>
-                    </li>
-                    <li>
-                        <a href="#my-work">My Work</a>
-                    </li>
-                    <li>
-                        <a href="#contact-me">Contact Me</a>
-                    </li>
-                </ul>
+                <nav>
+                    <ul className="flex-row">
+                        <li className="mx-2">
+                            <a href="#about">
+                                About me
+                            </a>
+                        </li>
+                        {pages.map((page) => (
+                        <li
+                            className="mx-1"
+                            key={page.name}
+                        >
+                            <span>
+                                {page.name}
+                            </span>
+                        </li>
+                        ))}
+                    </ul>
+                </nav>
             </header>
         </div>
     )
