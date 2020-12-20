b867ab08e82c5be97bf7b49a07e59c501ee985c8
Seth Uschuk <suschuk24@gmail.com>
2020-12-20 16:37:14 -0600

    Updates

---
+++ precache-manifest.4abbdff9fd5e2a5477825867dc6f9fea.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f2c4555d954d1eab431382d13cde03",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "6a5829a2cdcb91dac95d",
+    "url": "/react-portfolio/static/css/main.0b4e374e.chunk.css"
+  },
+  {
+    "revision": "b7e6a4f9d6ab4d5c294e",
+    "url": "/react-portfolio/static/js/2.b1a1fddf.chunk.js"
+  },
+  {
+    "revision": "5f2d6de39c39f8b6e46315c29574aa22",
+    "url": "/react-portfolio/static/js/2.b1a1fddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b76899e8a7f0d943f4",
+    "url": "/react-portfolio/static/js/3.dc45df8d.chunk.js"
+  },
+  {
+    "revision": "6a5829a2cdcb91dac95d",
+    "url": "/react-portfolio/static/js/main.fd57909d.chunk.js"
+  },
+  {
+    "revision": "06db7facb86efd2da16e",
+    "url": "/react-portfolio/static/js/runtime-main.c5a1cf7f.js"
+  },
+  {
+    "revision": "a3489421d3c240a38274fa8c5b714ab1",
+    "url": "/react-portfolio/static/media/cloud-rolodex-sample.a3489421.jpg"
+  },
+  {
+    "revision": "9e04918e8a2684a6b5395fba8f69992d",
+    "url": "/react-portfolio/static/media/container-bg.9e04918e.jpg"
+  },
+  {
+    "revision": "99688cd56d589663f8c1c4cf47cd1d71",
+    "url": "/react-portfolio/static/media/hero-bg.99688cd5.jpg"
+  },
+  {
+    "revision": "cc4abab718c6c16ab09811f72c4b3aa2",
+    "url": "/react-portfolio/static/media/movie-search-sample.cc4abab7.jpg"
+  },
+  {
+    "revision": "e790049c3bacb80ce42148c05bb64746",
+    "url": "/react-portfolio/static/media/profile-pic.e790049c.jpg"
+  },
+  {
+    "revision": "b219e4211220c96e3e6e1fb2bdaa4bda",
+    "url": "/react-portfolio/static/media/resume-sample.b219e421.jpg"
+  },
+  {
+    "revision": "55bb351f7a84a242ac12571e159850f8",
+    "url": "/react-portfolio/static/media/run-buddy-sample.55bb351f.jpg"
+  },
+  {
+    "revision": "8dbc773d2841a5a8dda5343b4ed348c9",
+    "url": "/react-portfolio/static/media/weather-forecast-sample.8dbc773d.jpg"
+  },
+  {
+    "revision": "92e46c2a12d264b4cd487e13d573b2e2",
+    "url": "/react-portfolio/static/media/zookeepr-sample.92e46c2a.jpg"
+  }
+]);
